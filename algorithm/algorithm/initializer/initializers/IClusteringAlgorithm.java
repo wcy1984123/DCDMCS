3fd5036619b2dd378a71e154aca9e6b6cdc321bb
Alex Wong <wangchiying@gmail.com>
2015-03-26 22:39:07 -0400

    Update Hierarchical Clustering Files

---
+++ algorithm/algorithm/initializer/initializers/IClusteringAlgorithm.java
@@ -0,0 +1,11 @@
+package initializer.initializers;
+
+/**
+ * Project: DCDMC
+ * Package: initializer.initializers
+ * Date: 26/Mar/2015
+ * Time: 21:19
+ * System Time: 9:19 PM
+ */
+public class IClusteringAlgorithm {
+}
